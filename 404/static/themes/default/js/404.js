c6bf6c31534bf9fcd333468548ba85aefbbd7d25
sophie-mao <shuying.mao@hpe.com>
2016-06-06 14:53:24 +0800

    clear 404 page

--- 404/static/themes/default/js/404.js
+++ 404/static/themes/default/js/404.js
@@ -4,10 +4,18 @@
 * ==========================================
 */
 
+$(".search-link").on('click', function(){
+  	alert("");
+ });
 
-var JSONObject= {
-"name":"Bill Gates",
-"street":"Fifth Avenue New York 666",
-"age":56,
-"phone":"555 1234567"};
+  // $(window).on('scroll', function(){
+  // 	alert("");
+  // });
+// var JSONObject= {
+// "name":"Bill Gates",
+// "street":"Fifth Avenue New York 666",
+// "age":56,
+// "phone":"555 1234567"};
 //document.getElementById("jname").innerHTML=JSONObject.name
+
+
